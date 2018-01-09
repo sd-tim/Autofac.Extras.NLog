6c3aa0eb2cbfbd9d1d34f3a1e627450113283fbf
Tim Wilde <timwilde@sd-taylor.co.uk>
2018-01-09 09:55:03 +0000

    repairing cluster-f of a merge after git flow.

--- src/AutoFac.NLogAdapter.NetCore20.Tests/When_using_the_callsite_in_a_layout.cs
+++ src/AutoFac.NLogAdapter.NetCore20.Tests/When_using_the_callsite_in_a_layout.cs
@@ -1,58 +1,170 @@
 ﻿namespace AutoFac.NLogAdapter.NetCore20.Tests
 {
    using System;
-   using System.Linq;
-   using System.Text.RegularExpressions;
-   using Autofac;
-   using FluentAssertions;
+   using System.Globalization;
    using NLog;
-   using NLog.Config;
-   using NLog.Targets;
-   using Xunit;
-   using Xunit.Abstractions;
+   using NUnit.Framework;
 
-   public class When_using_the_callsite_in_a_layout
+   [ TestFixture ]
+   public class When_using_the_callsite_in_a_layout: Test_fixture
    {
-      private readonly ITestOutputHelper output;
-      private readonly IContainer container;
-      private readonly MemoryTarget nlogMemoryTarget;
-
-      public When_using_the_callsite_in_a_layout( ITestOutputHelper output )
+      [ OneTimeSetUp ]
+      public void Set_up_test_fixture()
       {
-         this.output = output;
-
-         nlogMemoryTarget = new MemoryTarget { Layout = "${longdate} ${uppercase:${level}} ${callsite} ${message}${onexception:${newline}${exception}${newline}}" };
-         SimpleConfigurator.ConfigureForTargetLogging( nlogMemoryTarget, LogLevel.Trace );
-
-         var builder = new ContainerBuilder();
-         builder.RegisterModule<NLogModule>();
-         builder.RegisterType<SampleClassWithPropertyDependency>()
-                .Named<ISampleClass>( "property" )
-                .PropertiesAutowired();
-
-         container = builder.Build();
+         DeleteLogFile();
       }
 
-      [ Fact ]
-      public void Should_specify_the_correct_calling_type_and_method_names()
+      [ SetUp ]
+      public void Set_up_test_context()
       {
-         var logger = container.ResolveNamed<ISampleClass>( "property" ).GetLogger();
-         logger.Should().NotBeNull();
+         TestCode = $"{Guid.NewGuid():N}";
+      }
 
-         string testId = $"{Guid.NewGuid():N}";
-         logger.Error( testId );
+      [ Test ]
+      public void Should_specify_the_calling_type()
+      {
+         Logger.Log( LogLevel.Error, TestCode );
 
-         var lastLineOfLogs = nlogMemoryTarget.Logs.Last( x => x.EndsWith( testId ) );
+         VerifyLogMessage( TestCode );
+      }
 
-         var logLinePattern = new Regex( @"^(?<date>\d{4}-\d{2}-\d{2})\s(?<time>[\d:.]{13})\s(?<level>INFO|ERROR|WARNING)\s(?<callsite>[A-za-z0-9.]+)\s(?<msg>.*)$" );
+      [ Test ]
+      public void Should_work_for_level_and_generic_value()
+      {
+         Logger.Log<object>( LogLevel.Trace, new { message = TestCode } );
 
-         Match match = logLinePattern.Match( lastLineOfLogs );
-         match.Success.Should().BeTrue( because: "layout should match the regex" );
+         VerifyLogMessage( TestCode );
+      }
 
-         output.WriteLine( $"Callsite contains: {match.Groups[ "callsite" ].Value}" );
+      [ Test ]
+      public void Should_work_for_level_format_provider_and_generic_value()
+      {
+         Logger.Log<object>( LogLevel.Debug, CultureInfo.CurrentCulture, new { message = TestCode } );
 
-         match.Groups[ "callsite" ].Value.Should().Be( $"{GetType().FullName}.{nameof(Should_specify_the_correct_calling_type_and_method_names)}",
-                                                       because: "should use the actual calling method context." );
+         VerifyLogMessage( TestCode );
+      }
+
+      [ Test ]
+      public void Should_work_for_level_message_and_exception()
+      {
+         var exception = new Exception( TestCode );
+         Logger.Log( LogLevel.Fatal, TestCode, exception );
+
+         VerifyLogMessage( TestCode );
+         VerifyException( exception );
+      }
+
+      [ Test ]
+      public void Should_work_for_level_format_provider_message_and_arguments()
+      {
+         Logger.Log( LogLevel.Debug, CultureInfo.InstalledUICulture, "Testcode: {0}", new object[] { TestCode } );
+
+         VerifyLogMessage( TestCode );
+      }
+
+      [ Test ]
+      public void Should_work_for_level_message_and_args()
+      {
+         Logger.Log( LogLevel.Error, "Testcode: {0}", new object[] { TestCode } );
+
+         VerifyLogMessage( TestCode );
+      }
+
+      [ Test ]
+      public void Should_work_for_level_format_provider_message_and_generic_argument()
+      {
+         Logger.Log<string>( LogLevel.Info, CultureInfo.InstalledUICulture, "Testcode: {0}", TestCode );
+
+         VerifyLogMessage( TestCode );
+      }
+
+      [ Test ]
+      public void Should_work_for_level_format_provider_message_and_generic_argument_when_argument_is_an_exception()
+      {
+         var exception = new Exception( "Failed!" );
+         Logger.Log<Exception>( LogLevel.Fatal, CultureInfo.InstalledUICulture, TestCode, exception );
+
+         VerifyLogMessage( TestCode );
+         VerifyException( exception );
+      }
+
+      [ Test ]
+      public void Should_work_for_level_format_provider_message_and_two_generic_arguments()
+      {
+         var exception = new Exception( "Exception!" );
+         Logger.Log<string, Exception>( LogLevel.Warn, CultureInfo.InstalledUICulture, "Message: {0}, Exception: {1}", TestCode, exception );
+
+         VerifyLogMessage( TestCode );
+         VerifyException( exception );
+
+         TestCode = $"{Guid.NewGuid():N}";
+
+         Logger.Log<Exception, string>( LogLevel.Fatal, CultureInfo.InstalledUICulture, "Exception: {0}, Code: {1}", exception, TestCode );
+
+         VerifyLogMessage( TestCode );
+         VerifyException( exception );
+      }
+
+      [ Test ]
+      public void Should_work_for_level_message_and_two_generic_arguments()
+      {
+         var exception = new Exception( "Nope" );
+         Logger.Log<string, Exception>( LogLevel.Debug, "Code: {0}, Exception: {1}", TestCode, exception );
+
+         VerifyLogMessage( TestCode );
+         VerifyException( exception );
+
+         TestCode = $"{Guid.NewGuid():N}";
+
+         Logger.Log<Exception, string>( LogLevel.Error, "Exception: {0}, Code: {1}", exception, TestCode );
+
+         VerifyLogMessage( TestCode );
+         VerifyException( exception );
+      }
+
+      [ Test ]
+      public void Should_work_for_level_format_provider_message_and_three_generic_arguments()
+      {
+         var exception = new Exception( "Nu-uh!" );
+         Logger.Log<int, string, Exception>( LogLevel.Fatal, CultureInfo.InstalledUICulture, "Number: {0}, Code: {1}, Exception:{2}", 12, TestCode, exception );
+
+         VerifyLogMessage( TestCode );
+         VerifyException( exception );
+
+         TestCode = $"{Guid.NewGuid():N}";
+         Logger.Log<Exception, int, string>( LogLevel.Error, CultureInfo.InstalledUICulture, "Exception: {0}, Number: {1}, Code: {2}", exception, 12, TestCode );
+
+         VerifyLogMessage( TestCode );
+         VerifyException( exception );
+
+         TestCode = $"{Guid.NewGuid():N}";
+
+         Logger.Log<string, Exception, int>( LogLevel.Debug, CultureInfo.InstalledUICulture, "Code: {0}, Exception: {1}, Number: {2}", TestCode, exception, 12 );
+
+         VerifyLogMessage( TestCode );
+         VerifyException( exception );
+      }
+
+      [ Test ]
+      public void Should_work_for_level_message_and_three_generic_arguments()
+      {
+         var exception = new Exception( "Not today" );
+         Logger.Log<int, string, Exception>( LogLevel.Error, "Number: {0}, Code: {1}, Exception: {2}", 12, TestCode, exception );
+
+         VerifyLogMessage( TestCode );
+         VerifyException( exception );
+
+         TestCode = $"{Guid.NewGuid():N}";
+         Logger.Log<Exception, int, string>( LogLevel.Info, "Exception: {0}, Number: {1}, Code: {2}", exception, 12, TestCode );
+
+         VerifyLogMessage( TestCode );
+         VerifyException( exception );
+
+         TestCode = $"{Guid.NewGuid():N}";
+         Logger.Log<string, Exception, int>( LogLevel.Debug, "Code: {0}, Exception: {1}, Number: {2}", TestCode, exception, 12 );
+
+         VerifyLogMessage( TestCode );
+         VerifyException( exception );
       }
    }
 }
